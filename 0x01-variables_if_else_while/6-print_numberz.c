e19d1abd915bde2845e9ff8cf82096f7f707800d
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-09 20:49:21 +0000

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
