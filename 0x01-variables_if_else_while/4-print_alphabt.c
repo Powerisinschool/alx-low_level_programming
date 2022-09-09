f29ffceba7fcbd4caa445076d92fe5d92f95c8c5
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-09 20:41:43 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	return (0);
+}
