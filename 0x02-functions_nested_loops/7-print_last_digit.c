2691c4e558725a2a2ea898f51b774e64cd43aed1
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 18:40:50 +0000

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: 'integer n'
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int n)
+{
+	return (n % 10);
+}
