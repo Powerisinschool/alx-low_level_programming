d4e906f9171d60d4b5a1c856bbfa4dbadbdf8c75
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 20:27:31 +0000

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	for (i = 1; a1 < 1024; a1++)
+	{
+		if ((i * 3) < 1024)
+		{
+			sum += i * 3;
+		} else if ((i * 5) < 1024)
+		{
+			sum += i * 5;
+		} else
+		{
+			break;
+		}
+	}
+	return (0);
+}
