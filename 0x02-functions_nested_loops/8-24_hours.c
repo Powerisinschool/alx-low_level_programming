148ceaf5e447830c1bf3edcc1b00286057922ea6
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 19:27:15 +0000

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,24 +10,23 @@
 
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+	int i, j;
 
-	for (h1 = 0; h1 <= 2; h1++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (h2 = 0; h2 <= 3; h2++)
+		j = 0;
+		while (j < 60)
 		{
-			for (m1 = 0; m1 <= 5; m1++)
-			{
-				for (m2 = 0; m2 <= 9; m2++)
-				{
-					_putchar(h1 + '0');
-					_putchar(h2 + '0');
-					_putchar(':');
-					_putchar(m1 + '0');
-					_putchar(m2 + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
