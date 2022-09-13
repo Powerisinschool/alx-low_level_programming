db7427f11ef4cf7d677b37b45cc74c5a0fd37f2d
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 19:53:05 +0000

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,22 @@
 
 void print_to_98(int n)
 {
-	for (; n <= 97; n++)
+	if (n < 98)
 	{
-		printf("%d", n);
-		_putchar(',');
-		_putchar(' ');
+		for (; n < 98; n++)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	} else if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	printf("98\n");
 }
