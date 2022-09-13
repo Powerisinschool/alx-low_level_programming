2577883ee99d2666ee7c81b910191a3c3e166b89
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 16:57:20 +0000

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: 'int character'
+ *
+ * Description: 'the program's description'
+ *
+ * Return: 0 (False) or 1 (True)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
