77cf8d347325625241dea45cbfdcb4d2ff812c50
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 16:32:15 +0000

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Description: 'the program's description'
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
