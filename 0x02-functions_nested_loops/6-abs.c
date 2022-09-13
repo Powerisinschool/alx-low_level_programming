f116c41af53f3eb1b806afee561be8867f8177ee
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-13 18:30:33 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: 'rtfm abs'
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Absolute value of input number
+ */
+
+int _abs(int n)
+{
+	return (pow(pow(n, 2), 0.5));
+}
