8c4cbd7739d7030a09b1950985038b23187e0d1f
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-15 20:54:25 +0000

    Tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _isupper - check the code.
- *
- * Return: Always 0.
- */
-
+* _isupper - checks if c is an uppercase letter
+* @c: integer to test
+* Return: 1 for true, 0 for false
+*/
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
 }
