2d08a352397b67e2cb48eb318ad99a11dba44153
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-22 10:40:14 +0000

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,29 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer
+ */
 
 char *_strcat(char *dest, char *src)
 {
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = src[j];
+
+	return (dest);
 }
