c47828a2701bc58137e6ac2fb49a579b40f435c1
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-28 16:58:58 +0000

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+
+	return (x * _pow_recursion(x, y-1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ void _puts_recursion(char *s);
 int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 
 #endif
