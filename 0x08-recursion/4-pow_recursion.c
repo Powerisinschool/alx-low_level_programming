3267db59b6e3ddf7efa60650acf3712db85d2a45
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-09-28 17:00:51 +0000

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _pow_recursion - power of a number
+ * @x: base (int)
+ * @y: index (int)
+ * Return: x to power of y
  */
 
 int _pow_recursion(int x, int y)
@@ -11,5 +12,5 @@ int _pow_recursion(int x, int y)
 	if (y == 1)
 		return (x);
 
-	return (x * _pow_recursion(x, y-1);
+	return (x * _pow_recursion(x, y-1));
 }
