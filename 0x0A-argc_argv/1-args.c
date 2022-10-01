7bdb975debcbc22feef7a96fd42ffc4acfdd5177
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-10-01 04:20:49 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments as array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}
