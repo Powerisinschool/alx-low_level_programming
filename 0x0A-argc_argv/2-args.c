09ec4703b14c6c6363288be31cc4c7aea37176e2
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-10-01 04:29:46 +0000

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
