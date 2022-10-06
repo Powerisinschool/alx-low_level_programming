d206bc08dabcb6f1550c8998f7d6d65bdcdbc873
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-10-06 21:07:22 +0000

    Tasks

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strlen - counts the length of string.
+ * @s: Value string check.
+ * Return: String.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * string_nconcat - concatenate two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: bytes for s2 to use.
+ * Return: Only both strings.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	unsigned int i, len1, len2;
+	char *c;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	c = malloc(sizeof(char) * (len1 + n + 1));
+	if (c == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		c[i] = s1[i];
+	for (; i < len1 + n; i++)
+		c[i] = s2[i - len1];
+	c[len1 + n] = '\0';
+
+	return (c);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ *_calloc -allocates memory for an array
+ *@nmemb: number of element in the array
+ *@size: bytes for each position in the array
+ *Return: pointer void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *x;
+	unsigned int a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	x = malloc(nmemb * size);
+	if (x == NULL)
+		return (NULL);
+
+	for (a = 0; a < (nmemb * size); a++)
+		x[a] = 0;
+
+	return (x);
+
+}
