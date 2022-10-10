26ad7c7661bf4d7bae401779fcf409dff107b2c8
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-10-10 19:49:05 +0000

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,18 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner
+ */
+
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
