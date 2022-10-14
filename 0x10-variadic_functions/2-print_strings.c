ad5ac4eb8e5f3949c6d4a980bee478d73924d24c
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2022-10-14 20:03:18 +0100

    /home/tolu/.local/bin/gitter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_stringe - print a list of strings
+ *
+ * @separator: The separator between strings
+ * @n: count of arguments
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	char *s;
+	u_int i = 1;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	if (n >= 1)
+		printf("%s", va_arg(ap, char*));
+
+	for (; i < n; i++)
+	{
+		s = va_arg(ap, char*);
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s%s", separator, s);
+	}
+	putchar('\n');
+
+	va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /*VARIADIC_FUNCTIONS*/
