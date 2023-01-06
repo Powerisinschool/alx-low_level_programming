62679cb02042bd47cdbf864addc19599a7e58698
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-06 22:03:21 +0100

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,6 +15,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = ht->array[index];
 	hash_node_t *new_node;
 
+	if (ht == NULL || *key == '\n' || *value == '\n')
+		return (0);
+
 	if (node == NULL)
 	{
 		new_node = create_node(key, value);
