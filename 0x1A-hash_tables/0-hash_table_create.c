1bfd465281bf6837b92e55b71cfdb2a9c9f82d30
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-06 19:06:00 +0100

    Task 0 fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,23 +1,42 @@
 #include "hash_tables.h"
 
-unsigned long hash_function(char* str, unsigned long int size) {
-  unsigned long i = 0;
-  int j;
-  for (j=0; str[j]; j++)
-    i += str[j];
-  return i % size;
+/**
+ * hash_function - Entry point
+ * @str: string
+ * @size: int
+ * Description: 'Return hash of a string'
+ *
+ * Return: Hash (Success)
+ */
+unsigned long hash_function(char *str, unsigned long int size)
+{
+	unsigned long i = 0;
+	int j;
+
+	for (j = 0; str[j]; j++)
+		i += str[j];
+	return (i % size);
 }
 
-hash_table_t *hash_table_create(unsigned long int size) {
-  unsigned long int i;
-  hash_table_t* table;
+/**
+ * hash_table_create - Entry point
+ *
+ * @size: int
+ * Description: 'Create an empty hash table'
+ *
+ * Return: Pointer to hash table (Success) or NULL (failure)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
 
-  (void) size;
+	(void) size;
 
-  table = (hash_table_t*) malloc (sizeof(hash_table_t));
-  table->size = size;
-  table->array = (hash_node_t**) calloc (table->size, sizeof(hash_node_t*));
-  for (i=0; i<table->size; i++)
-    table->array[i] = NULL;
-  return table;
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	table->size = size;
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+	return (table);
 }
