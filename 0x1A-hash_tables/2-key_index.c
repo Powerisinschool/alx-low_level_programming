65f8fe4e7342e4cf897523aaa883c01d4963c201
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-06 20:54:04 +0100

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,11 +2,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  unsigned long int index;
+	unsigned long int index = hash_djb2(key) % size;
 
-  (void) size;
-
-  index = hash_djb2(key);
-
-  return (index);
+	return (index);
 }
