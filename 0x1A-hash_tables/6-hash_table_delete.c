faa657f0df13303d3a855097eb2be72c050aabdb
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-07 17:41:41 +0100

    Task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,5 +2,28 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_print(ht);
+	unsigned long int i;
+	hash_node_t *node;
+	hash_node_t *next;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			next = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+	free(node);
+	free(next);
+	return;
 }
