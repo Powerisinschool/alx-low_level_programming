77da3e52f2a59a9ca7d2ef25fdda0c99caffa9b8
Powerisinschool <toolulopeolagunju@gmail.com>
2023-01-07 08:47:42 +0100

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - retrieves a value associated with a key.
+ * @ht: the hash table you want to look into
+ * Return: void
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
