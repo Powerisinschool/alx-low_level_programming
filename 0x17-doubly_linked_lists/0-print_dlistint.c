aa74ab767b53b68534ce6b0c91851d1115aaa63b
femidamilola <femi.adebayo1998@gmail.com>
2023-01-08 18:44:47 +0100

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,5 +2,20 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	return (1);
+	size_t i = 1;
+	dlistint_t *curr = (dlistint_t *) h;
+
+	while (curr != NULL)
+	{
+		curr = curr->next;
+		i++;
+	}
+
+	curr = (dlistint_t *) h;
+	while (curr != NULL)
+	{
+		curr = curr->prev;
+		i++;
+	}
+	return (i);
 }
