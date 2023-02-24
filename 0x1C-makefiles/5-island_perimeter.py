c4062224e45e0398471053b5a15a97edd9c578e5
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-24 18:45:20 +0100

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,19 +4,24 @@
 
 
 def island_perimeter(grid):
-    """Return the perimeter of combined islands in a grid"""
-    perimeter = 0
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid[i])):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                sum = 4
-                if grid[i-1][j] == 1:
-                    sum -= 1
-                if grid[i+1][j] == 1:
-                    sum -= 1
-                if grid[i][j-1] == 1:
-                    sum -= 1
-                if grid[i][j+1] == 1:
-                    sum -= 1
-                perimeter += sum
-    return (perimeter)
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
