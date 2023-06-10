31b7c7e2b27719bf8ca7f7208f92ac152f019857
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2023-06-10 05:27:32 -0700

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/Powerisinschool/alx-low_level_programming/raw/master/0x18-dynamic_libraries/rand_hijack.so
-export LD_PRELOAD=./rand_hijack.so
+wget https://github.com/Powerisinschool/alx-low_level_programming/raw/master/0x18-dynamic_libraries/rand_hijack.so -P /tmp
+export LD_PRELOAD=/tmp/rand_hijack.so
